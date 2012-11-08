c1ea0b648b211b60a46b5410b98131d3373ada31
kim <sunlibing@limei.com>
2012-11-08 10:51:03 +0800

    compatible with hadoop verison cdh4

--- lang/java/avro/src/topsoft/com/topsoft/botspider/avro/mapreduce/ConfigSchemaData.java
+++
@@ -1,8 +0,0 @@
-package com.topsoft.botspider.avro.mapreduce;
-
-import org.apache.avro.Schema;
-import org.apache.hadoop.conf.Configuration;
-
-public interface ConfigSchemaData {
-  public Schema getSchema(Configuration conf);
-}
